409c75dccfdcb6012fde235989b88959249a776e
PetrBelmst <PetrBelmst@users.noreply.github.com>
2020-05-19 03:48:41 +0300

    sort methods changes

--- src/main/java/com/example/demo/controller/ArtefactController.java
+++ src/main/java/com/example/demo/controller/ArtefactController.java
@@ -9,7 +9,9 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -58,6 +60,41 @@ public class ArtefactController implements AnyEntityObjectController <Artefact, 
         return artefactService.read(id).isPresent()
                 ? new ResponseEntity<>(HttpStatus.NOT_MODIFIED)
                 : new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity <List<Artefact>> findByCategory(@RequestParam String category) {
+        return ResponseEntity.ok(artefactService.findByCategory(category));
+    }
+
+    @GetMapping
+    public ResponseEntity <Artefact> findByUserID(@RequestParam String userID){
+        return ResponseEntity.ok(artefactService.findByUser(userID));
+    }
+
+    @GetMapping
+    public ResponseEntity <List<Artefact>> findByDescription(@RequestParam String description){
+        return ResponseEntity.ok(artefactService.findByDescription(description));
+    }
+
+    @GetMapping
+    public ResponseEntity <List<Artefact>> findByCommentListContaining_Content(@RequestParam String content){
+        return ResponseEntity.ok(artefactService.findByComment(content));
+    }
+
+    @GetMapping("/sort_by_created")
+    public ResponseEntity <List<Artefact>> findByOrderByCreatedAsc(){
+        return ResponseEntity.ok(artefactService.sortByDate());
+    }
+
+    @GetMapping("/sort_by_category")
+    public ResponseEntity <List<Artefact>> findByOrderByCategoryAsc(){
+        return ResponseEntity.ok(artefactService.sortByCategory());
+    }
+
+    @GetMapping("/sort_by_user_id")
+    public ResponseEntity <List<Artefact>> findByOrderByUserIDAsc(){
+        return ResponseEntity.ok(artefactService.sortByUser());
     }
 
 
